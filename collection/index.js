1d71ac4bf04a870f5cb0b4769643737f765283d6
Erik Trom <erik.trom@gmail.com>
2013-11-30 00:20:36 -0800

    refactor collection

--- collection/index.js
+++ collection/index.js
@@ -1,16 +1,15 @@
-var util      = require('util');
-var path      = require('path');
-var Generator = require('../lib/generator');
+'use strict';
+var util = require('util');
+var yeoman = require('yeoman-generator');
+var path = require('path'); // duplication
 
-var CollectionGenerator = module.exports = function () {
-  Generator.apply(this, arguments);
+var CollectionGenerator = module.exports = function CollectionGenerator(args, options, config) {
+  yeoman.generators.NamedBase.apply(this, arguments);
+  this.sourceRoot(path.join(__dirname, '../templates')); // duplication
 };
 
-util.inherits(CollectionGenerator, Generator);
+util.inherits(CollectionGenerator, yeoman.generators.NamedBase);
 
-CollectionGenerator.prototype._name  = 'collection';
-CollectionGenerator.prototype.askFor = Generator.prototype.askFor;
-
-CollectionGenerator.prototype.createCollection = function () {
-  this._renderTemplate('js/collections');
+CollectionGenerator.prototype.files = function files() {
+  this.template('collection.js', path.join('js/collections', this._.dasherize(this.name) + '.js'));
 };
