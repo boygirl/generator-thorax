d6b72d8d801efc68603cf4a0354e04e1724a9b02
Erik Trom <erik.trom@gmail.com>
2013-11-30 13:25:31 -0800

    mad cleanup of generator test yo

---
+++ test/sub-generators.spec.js
@@ -0,0 +1,256 @@
+/* global describe, beforeEach, it, before */
+var path    = require('path');
+var chai = require('chai');
+var expect = chai.expect;
+var helpers = require('yeoman-generator').test;
+var fs = require('fs');
+var sharedExamples = require('./shared-examples');
+
+var assert = chai.assert;
+
+helpers.assertNoFile = function (file, reg) {
+  var here = fs.existsSync(file);
+  assert.ok(!here, file + ' DOES exist, something is wrong');
+
+  if (!reg) {
+    return assert.ok(!here);
+  }
+
+  var body = fs.readFileSync(file, 'utf8');
+  assert.ok(!reg.test(body), file + ' DID MATCH, HOLD THE PHONE: match \'' + reg + '\'.');
+};
+
+helpers.assertFileHasNoContent = function(file, reg) {
+  var here = fs.existsSync(file);
+  assert.ok(here, file + ' does exist, we expected that');
+
+  if (!reg) {
+    return assert.fail('You must provide content via regex for this helper');
+  }
+
+  var body = fs.readFileSync(file, 'utf8');
+  assert.ok(!reg.test(body), file + ' DID MATCH, STAHP!, control flow the following content or fix the test: match \'' + reg + '\'.');
+};
+
+function requireOption(option, message) {
+  if (typeof option === 'undefined') { throw new Error(message); }
+  return option;
+};
+
+describe('thorax sub generators', function () {
+
+  beforeEach(function (done) {
+    helpers.testDirectory(path.join(__dirname, 'temp'), function (err) {
+      if (err) { return done(err); }
+
+      this.app = helpers.createGenerator('thorax:app', ['../../app'], 'test');
+      this.app.options['skip-install'] = true;
+
+      helpers.mockPrompt(this.app, {
+        'newDirectory': false,
+        'starterApp': "None",
+        'styleProcessor': "none",
+        'includeBootstrap': false,
+        'includeCoffeeScript': requireOption(this.includeCoffeeScript, "Forgot to provide includeCoffeeScript"),
+        'useZepto': false
+      });
+
+      this.app.run({}, done);
+    }.bind(this));
+  });
+
+  describe('yo thorax:router foo', function () {
+    describe('When app is JS based(not CS)', function () {
+      before(function() { this.includeCoffeeScript = false; });
+
+      it('generates a Backbone router in javascript', function (done) {
+      var router = helpers.createGenerator('thorax:router', ['../../router'], ['foo']);
+
+      router.run([], function () {
+        helpers.assertFiles([
+          ['js/routers/foo.js', /Backbone.Router.extend\(\{/],
+          ['js/routers/foo.js', /function \(Backbone, RootView\)/]
+        ]);
+        done();
+      });
+    });
+
+
+    });
+
+    describe('When app is CS based', function () {
+      before(function() { this.includeCoffeeScript = true; });
+
+      it('generates a backbone router in coffeescript', function (done) {
+        var router = helpers.createGenerator('thorax:router', ['../../router'], 'foo');
+
+        router.run([], function () {
+          helpers.assertFiles([
+            ['js/routers/foo.coffee', /Backbone.Router.extend/],
+            ['js/routers/foo.coffee', /\(Backbone, RootView\) ->/]
+          ]);
+          done();
+        });
+      });
+
+    });
+  });
+
+  describe('yo thorax:view foo', function () {
+    describe('when app is JS based(not CS)', function () {
+
+      before(function() { this.includeCoffeeScript = false; });
+
+      it('generates a Thorax view', function (done) {
+        var view = helpers.createGenerator('thorax:view', ['../../view'], ['foo']);
+
+        view.run([], function () {
+          helpers.assertFiles([
+            ['js/views/foo.js', /View.extend\(\{/],
+            ['js/views/foo.js', /name: 'foo'/],
+            'js/templates/foo.hbs'
+          ]);
+          done();
+        });
+      });
+    });
+
+    describe('when app is CS based', function () {
+      before(function() { this.includeCoffeeScript = true; });
+
+      it('generates a Thorax view', function (done) {
+        var view = helpers.createGenerator('thorax:view', ['../../view'], ['foo']);
+
+        view.run([], function () {
+          helpers.assertFiles([
+            ['js/views/foo.coffee', /View.extend/],
+            ['js/views/foo.coffee', /name: 'foo'/],
+            ['js/views/foo.coffee', /'hbs!templates\/foo'/],
+            ['js/views/foo.coffee', /'cs!view'/],
+            'js/templates/foo.hbs'
+          ]);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('yo thorax:model', function () {
+    describe('when app is JS based(not CS)', function () {
+      before(function() { this.includeCoffeeScript = false; });
+
+      it('generates a Thorax model', function (done) {
+        var model = helpers.createGenerator('thorax:model', ['../../model'], ['foo']);
+
+        model.run([], function () {
+          helpers.assertFiles([
+            ['js/models/foo.js', /Model.extend\(\{/],
+            ['js/models/foo.js', /name: 'foo'/]
+          ]);
+          done();
+        });
+      });
+
+    });
+
+    describe('when app is CS based', function () {
+      before(function() { this.includeCoffeeScript = true; });
+
+      it('generates a Thorax model', function (done) {
+        var model = helpers.createGenerator('thorax:model', ['../../model'], ['foo']);
+
+        model.run([], function () {
+          helpers.assertFiles([
+            ['js/models/foo.coffee', /Model.extend/],
+            ['js/models/foo.coffee', /name: 'foo'/]
+          ]);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('yo thorax:collection foo', function () {
+    describe('when app is JS based(not CS)', function () {
+
+      before(function() { this.includeCoffeeScript = false; });
+
+      it('generates a Thorax collection', function (done) {
+        var collection = helpers.createGenerator('thorax:collection', ['../../collection'], ['foo']);
+
+        collection.run([], function () {
+          helpers.assertFiles([
+            ['js/collections/foo.js', /Collection.extend\(\{/],
+            ['js/collections/foo.js', /name: 'foo'/]
+          ]);
+          done();
+        });
+      });
+
+    });
+
+    describe('when app is CS based', function () {
+      before(function() { this.includeCoffeeScript = true; });
+
+      it('generates a Thorax collection', function (done) {
+        var collection = helpers.createGenerator('thorax:collection', ['../../collection'], ['foo']);
+
+        collection.run([], function () {
+          helpers.assertFiles([
+            ['js/collections/foo.coffee', /Collection.extend/],
+            ['js/collections/foo.coffee', /name: 'foo'/]
+          ]);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('yo thorax:collection-view fooBar', function () {
+    describe('when app is JS based(not CS)', function () {
+
+      before(function() { this.includeCoffeeScript = false; });
+
+      it('generates a Thorax collection view', function (done) {
+        var collectionView = helpers.createGenerator('thorax:collection-view', ['../../collection-view'], ['fooBar']);
+
+        collectionView.run([], function () {
+          helpers.assertFiles([
+            ['js/views/foo-bar.js', /CollectionView.extend\(\{/],
+            ['js/views/foo-bar.js', /name: 'fooBar'/],
+            ['js/views/foo-bar.js', /'hbs!templates\/foo-bar'/],
+            ['js/views/foo-bar.js', /'hbs!templates\/foo-bar-item'/],
+            ['js/views/foo-bar.js', /'hbs!templates\/foo-bar-empty'/],
+            'js/templates/foo-bar.hbs',
+            'js/templates/foo-bar-item.hbs',
+            'js/templates/foo-bar-empty.hbs'
+          ]);
+          done();
+        });
+      });
+
+    });
+
+    describe('when the app is CS based', function () {
+      before(function() { this.includeCoffeeScript = true; });
+      it('generates a Thorax collection view', function (done) {
+        var collectionView = helpers.createGenerator('thorax:collection-view', ['../../collection-view'], ['fooBar']);
+
+        collectionView.run([], function () {
+          helpers.assertFiles([
+            ['js/views/foo-bar.coffee', /CollectionView.extend/],
+            ['js/views/foo-bar.coffee', /name: 'fooBar'/],
+            ['js/views/foo-bar.coffee', /'hbs!templates\/foo-bar'/],
+            ['js/views/foo-bar.coffee', /'hbs!templates\/foo-bar-item'/],
+            ['js/views/foo-bar.coffee', /'hbs!templates\/foo-bar-empty'/],
+            'js/templates/foo-bar.hbs',
+            'js/templates/foo-bar-item.hbs',
+            'js/templates/foo-bar-empty.hbs'
+          ]);
+          done();
+        });
+      });
+    });
+
+  });
+});
