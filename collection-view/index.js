06e36a8fbf505ebc225395ec6a47c09db7c4bba0
Erik Trom <erik.trom@gmail.com>
2013-11-30 01:45:02 -0800

    cleanup

--- collection-view/index.js
+++ collection-view/index.js
@@ -1,6 +1,5 @@
 'use strict';
 var util = require('util');
-var path = require('path');
 var ScriptBase = require('../script-base.js');
 
 var Generator = module.exports = function Generator() {
@@ -10,11 +9,9 @@ var Generator = module.exports = function Generator() {
 util.inherits(Generator, ScriptBase);
 
 Generator.prototype.files = function files() {
-  // next line same as view/index.js but prefer minimal duplication over inheritance
-  this.write('js/templates/' + this._.dasherize(this.name) + '.hbs', '');
-
-  // files specific to collection view
   this.appTemplate('collection-view', 'views');
+  // write out blank templates
+  this.write('js/templates/' + this._.dasherize(this.name) + '.hbs', '');
   this.write('js/templates/' + this._.dasherize(this.name) + '-item.hbs', '');
   this.write('js/templates/' + this._.dasherize(this.name) + '-empty.hbs', '');
 };
--- view/index.js
+++ view/index.js
@@ -1,6 +1,5 @@
 'use strict';
 var util = require('util');
-var path = require('path');
 var ScriptBase = require('../script-base.js');
 
 var Generator = module.exports = function Generator() {
@@ -11,5 +10,6 @@ util.inherits(Generator, ScriptBase);
 
 Generator.prototype.files = function files() {
   this.appTemplate('view', 'views');
+  // write out blank template
   this.write('js/templates/' + this._.dasherize(this.name) + '.hbs', '');
 };
